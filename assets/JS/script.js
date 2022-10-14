6344c560b4f7595b55091773b1fabb0fb1f0775e
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-14 12:44:14 -0400

    added listener to save button, stored name and displayed name in the favorites card

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,7 +7,6 @@ var marvelCharNameUrl = "https://gateway.marvel.com:443/v1/public/characters?ord
 var vineRequestUrl = "https://comicvine.gamespot.com/api/characters/?api_key=84b5c7942b49eb33ea48d45716e0e2336811cd22&format=JSON&limit=10"
 
 
-
 $( function() {
   $( "#char-search" ).autocomplete({
     source: marvelChars,
@@ -38,6 +37,46 @@ function getResultsMarvel(selectedChar)
   })
   // console.log("Character Name: " + charName);
 }
+
+
+// save to favorites button
+
+var saveButton = $('#save-fav');
+var charSearchEl = $('#char-search');
+var charName1 = $('#char-name-1');
+
+
+var favoriteCharacters = [];
+
+// get data from local storage 
+function init () {
+  // get items from local storage
+    // if no favorites
+      // clear contents of "cards-container"
+      // replace text of #instructions with "Save a character as a favorite to .."
+    // if favorites
+
+      // loop through array of favorites 0 - 3 (4 max)
+      // add text in char-name-1, etc. - the character's name
+      // add thumbnail char-image-1 - set src attribute (with the URL for the image from the API) and alt attribute
+}
+
+function saveFavorite() {
+    console.log(charSearchEl.val());
+    favoriteCharacters.unshift(charSearchEl.val());
+    console.log(favoriteCharacters);
+    localStorage.setItem("favoriteCharacters", JSON.stringify(favoriteCharacters));
+    var currentFavs = JSON.parse(localStorage.getItem("favoriteCharacters"))
+      // add name to favorite card #1
+      charName1.text(currentFavs[0]);
+      // add image to favorite card #1
+      
+
+}
+
+
+// run function when button is clicked
+saveButton.on('click', saveFavorite);
 
 // DATA
 
